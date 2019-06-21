a1f0a488f99982e166ad8a5330575189fb8a02a4
wang.feng <wang_feng@cygames.co.jp>
2019-06-21 18:59:33 +0900

    Added chapter 8.

---
+++ Assets/Cookbook/Scripts/8. Screen Effects/TestMyRenderImage.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TestMyRenderImage : MonoBehaviour
+{
+    #region Variables
+
+    public Shader curShader;
+    public float grayScaleAmount = 1.0f;
+    private Material curMaterial;
+
+    #endregion
+
+    #region Properties
+
+    Material material
+    {
+        get
+        {
+            if (curMaterial == null)
+            {
+                curMaterial = new Material(curShader);
+                curMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return curMaterial;
+        }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+        if (!curShader && !curShader.isSupported)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (curShader != null)
+        {
+            material.SetFloat("_LuminosityAmount", grayScaleAmount);
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    private void Update()
+    {
+        grayScaleAmount = Mathf.Clamp(grayScaleAmount, 0f, 1f);
+    }
+
+    private void OnDisable()
+    {
+        if (curMaterial)
+        {
+            DestroyImmediate(curMaterial);
+        }
+    }
+}
---
+++ Assets/Cookbook/Shader/8. Screen Effects/ImageEffect.shader
@@ -0,0 +1,60 @@
+﻿Shader "MyShaders/ImageEffect"
+{
+    Properties
+    {
+        _MainTex ("Base Texture", 2D) = "white" {}
+		_LuminosityAmount("GrayScale Amount", Range(0, 1)) = 1.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			fixed _LuminosityAmount;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 renderTexCol = tex2D(_MainTex, i.uv);
+
+				// apply th eluminosity values to our render texture.
+				float luminosity = 0.299 * renderTexCol.r + 0.587 * renderTexCol.g + 0.114 * renderTexCol.b;
+				fixed4 col = lerp(renderTexCol, luminosity, _LuminosityAmount);
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
