8295343db4513168db7200507788b3d58a79e0ed
wang.feng <wang_feng@cygames.co.jp>
2019-10-03 12:56:59 +0900

    Implemented motion blur

---
+++ Assets/Scripts/PostEffect/MotionBlur.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionBlur : PostEffectBase
+{
+    public Shader motionBlurShader;
+    private Material motionBlurMat = null;
+    public Material material
+    {
+        get
+        {
+            motionBlurMat = CreateMaterial(motionBlurShader, motionBlurMat);
+            return motionBlurMat;
+        }
+    }
+
+    [Range(0.0f, 1f)]
+    public float blurAmount = 0.5f;
+
+    // 保存之前图像叠加的结果
+    private RenderTexture accumulationTexture;
+
+    private void OnDisable()
+    {
+        DestroyImmediate(accumulationTexture);
+    }
+
+    /// <summary>
+    /// Version 3.
+    /// Use iterations for large blur.
+    /// </summary>
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            if (accumulationTexture == null ||
+                accumulationTexture.width != source.width ||
+                accumulationTexture.height != source.height)
+            {
+                DestroyImmediate(accumulationTexture);
+
+                accumulationTexture = new RenderTexture(source.width, source.height, 0);
+                // 表示这个变量不会显示在Hierarchy中, 也不会保存到场景中
+                accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumulationTexture);
+            }
+
+            // We are accumulating motion over frames without clear/discard by design, so silence any performance warnings from Unity
+            // 表明需要进行一个渲染纹理的恢复操作（restore operation）。恢复操作发生在渲染到纹理而该纹理又没有被提前清空或销毁的情况下。
+            accumulationTexture.MarkRestoreExpected();
+
+            material.SetFloat("_BlurAmount", 1f - blurAmount);
+
+
+            Graphics.Blit(source, accumulationTexture, material);
+            Graphics.Blit(accumulationTexture, destination);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ Assets/Shaders/PostEffect/MotionBlur.shader
@@ -0,0 +1,86 @@
+﻿Shader "MyShaders/MotionBlur"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_BlurAmount("Blur Amount", Float) = 1.0
+	}
+
+	SubShader
+	{
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+		half4 _MainTex_TexelSize;
+		float  _BlurAmount;
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			half2 texcoord : TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float4 pos : SV_POSITION;
+			half2 uv : TEXCOORD0;
+		};
+
+		v2f	vert(appdata v)
+		{
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			o.uv = v.texcoord;
+
+			return o;
+		}
+		// 使得混合时不受透明值的影响
+		fixed4 fragRGB(v2f i) : SV_Target
+		{
+			return fixed4(tex2D(_MainTex, i.uv).rgb, _BlurAmount);
+		}
+
+		fixed4 fragA(v2f i) : SV_Target
+		{
+			return tex2D(_MainTex, i.uv);
+		}
+
+
+		ENDCG
+
+		ZTest Always
+		Cull Off
+		ZWrite Off
+
+		// pass 0
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha
+			ColorMask RGB
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment fragRGB
+
+			ENDCG
+		}
+		// pass 1
+		Pass
+		{
+			Blend One Zero
+			//Blend SrcAlpha OneMinusSrcAlpha
+			// ColorMask可以让我们制定渲染结果的输出通道，而不是通常情况下的RGBA这4个通道全部写入。
+			// 可选参数是 RGBA 的任意组合以及 0，这将意味着不会写入到任何通道，可以用来单独做一次Z测试，而不将结果写入颜色通道
+			ColorMask A
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment fragA
+
+			ENDCG
+		}
+	}
+	Fallback Off
+}
